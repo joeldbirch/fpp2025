df25d99e9d8f4eb3164247a6f0784053a152b3e3
Joel Birch <joeldbirch@gmail.com>
2018-03-21 11:39:58 +1100

    Add missing layout.

---
+++ src/layouts/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import TheWrap from '../components/TheWrap/TheWrap.js'
+import TheBanner from '../components/TheBanner/TheBanner.js'
+import TheMenu from '../components/TheMenu/TheMenu.js'
+import TheFooter from '../components/TheFooter/TheFooter.js'
+
+import '../sass/_generic.global.scss'
+
+
+export default ({children}) => (
+  <TheWrap>
+    <Helmet>
+      <meta name="robots" content="noindex" />
+    </Helmet>
+
+    <TheBanner>
+      <TheMenu />
+    </TheBanner>
+
+    <div className="content">
+      {children()}
+    </div>
+
+    <TheFooter />
+  </TheWrap>
+)
