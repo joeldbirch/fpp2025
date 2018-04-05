b2fe457a69d0fb8197dee7daa8a155467e60fa91
Joel Birch <joeldbirch@gmail.com>
2018-04-05 15:20:04 +1000

    Extract wufoo form to utility file.

--- src/templates/page.js
+++ src/templates/page.js
@@ -5,17 +5,12 @@ import BaseSideColumn from '../components/BaseSideColumn'
 import BaseContentWrap from '../components/BaseContentWrap'
 import SidebarWidgetFactory from '../components/SidebarWidgetFactory'
 import {sortByObjProp} from '../utils/helpers'
+import wufooForm from '../utils/wufooForm'
 
 class Template extends Component {
-  constructor() {
-    super()
-  }
-
   componentDidMount() {
     let {slug} = this.props.data.wordpressPage
-    if (slug === 'contact') {
-      var rzgkhvs1b7d9hw;(function(d, t) {var s = d.createElement(t), options = {'userName':'fppdesign', 'formHash':'rzgkhvs1b7d9hw', 'autoResize':true, 'height':'437', 'async':true, 'host':'wufoo.eu', 'header':'show', 'ssl':true}; s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.eu/scripts/embed/form.js'; s.onload = s.onreadystatechange = function() {var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return; try { rzgkhvs1b7d9hw = new WufooForm();rzgkhvs1b7d9hw.initialize(options);rzgkhvs1b7d9hw.display(); } catch (e) {}}; var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr); })(document, 'script');
-    }
+    wufooForm(slug)
   }
 
   render() {
---
+++ src/utils/wufooForm.js
@@ -0,0 +1,6 @@
+export default function (pageSlug) {
+  if (pageSlug === 'contact') {
+    // wufoo script…
+    var rzgkhvs1b7d9hw;(function(d, t) {var s = d.createElement(t), options = {'userName':'fppdesign', 'formHash':'rzgkhvs1b7d9hw', 'autoResize':true, 'height':'437', 'async':true, 'host':'wufoo.eu', 'header':'show', 'ssl':true}; s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.eu/scripts/embed/form.js'; s.onload = s.onreadystatechange = function() {var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return; try { rzgkhvs1b7d9hw = new WufooForm();rzgkhvs1b7d9hw.initialize(options);rzgkhvs1b7d9hw.display(); } catch (e) {}}; var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr); })(document, 'script');
+  }
+}
