75335893db27925d03f977d488edf7a5bbbab255
Joel Birch <joeldbirch@gmail.com>
2018-04-05 15:00:11 +1000

    Wufoo form works.

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import TheHeading from '../components/TheHeading'
 import BaseMainColumn from '../components/BaseMainColumn'
 import BaseSideColumn from '../components/BaseSideColumn'
@@ -6,22 +6,35 @@ import BaseContentWrap from '../components/BaseContentWrap'
 import SidebarWidgetFactory from '../components/SidebarWidgetFactory'
 import {sortByObjProp} from '../utils/helpers'
 
-export default function Template(props) {
-  let {title, content, acf} = props.data.wordpressPage
-  let {sidebarItems} = props.pathContext
-  let sortedSidebarItems = sortByObjProp(acf.page_sidebar_items, sidebarItems, 'wordpress_id')
+class Template extends Component {
+  constructor() {
+    super()
+  }
 
-  return (
-    <BaseContentWrap>
-      <TheHeading>{title}</TheHeading>
-      <BaseMainColumn>
-        {content}
-      </BaseMainColumn>
-{      <BaseSideColumn>
-        <SidebarWidgetFactory nodes={sortedSidebarItems} />
-      </BaseSideColumn>}
-    </BaseContentWrap>
-  )
+  componentDidMount() {
+    let {slug} = this.props.data.wordpressPage
+    if (slug === 'contact') {
+      var rzgkhvs1b7d9hw;(function(d, t) {var s = d.createElement(t), options = {'userName':'fppdesign', 'formHash':'rzgkhvs1b7d9hw', 'autoResize':true, 'height':'437', 'async':true, 'host':'wufoo.eu', 'header':'show', 'ssl':true}; s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.eu/scripts/embed/form.js'; s.onload = s.onreadystatechange = function() {var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return; try { rzgkhvs1b7d9hw = new WufooForm();rzgkhvs1b7d9hw.initialize(options);rzgkhvs1b7d9hw.display(); } catch (e) {}}; var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr); })(document, 'script');
+    }
+  }
+
+  render() {
+    let {title, content, acf, slug} = this.props.data.wordpressPage
+    let {sidebarItems} = this.props.pathContext
+    let sortedSidebarItems = sortByObjProp(acf.page_sidebar_items, sidebarItems, 'wordpress_id')
+
+    return (
+      <BaseContentWrap>
+        <TheHeading>{title}</TheHeading>
+        <BaseMainColumn>
+          {content}
+        </BaseMainColumn>
+        <BaseSideColumn>
+          <SidebarWidgetFactory nodes={sortedSidebarItems} />
+        </BaseSideColumn>
+      </BaseContentWrap>
+    )
+  }
 }
 
 export const pageQuery = graphql`
@@ -29,9 +42,11 @@ export const pageQuery = graphql`
     wordpressPage(id: { eq: $id }) {
       title
       content
+      slug
       acf {
         page_sidebar_items
       }
     }
   }
 `
+export default Template
