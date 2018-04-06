d701fc5a3e8adc9cf93964a4798eccee23509471
Joel Birch <joeldbirch@gmail.com>
2018-04-06 14:18:54 +1000

    Use https for the WordPress admin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
           */
         baseUrl: "admin.fppdesign.com.au",
         // The protocol. This can be http or https.
-        protocol: "http",
+        protocol: "https",
         // Indicates whether the site is hosted on wordpress.com.
         // If false, then the asumption is made that the site is self hosted.
         // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
