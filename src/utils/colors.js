4a69464751d647f3799b85e5e311ccfd9b49e6c6
Joel Birch <joeldbirch@gmail.com>
2018-04-07 12:42:35 +1000

    Darker link colours.

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,5 +1,6 @@
 export const brandColor = '#599e01'
 export const dullBrandColor = '#599e01'
+export const darkBrandColor = '#467d00'
 export const accentColor = '#EB9000'
-export const linkColor = dullBrandColor
+export const linkColor = darkBrandColor
 export const baseFontColor = '#444'
