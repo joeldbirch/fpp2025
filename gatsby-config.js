09c9207993e1cbe43f7f66f94b2336bdd4ca18ce
Joel Birch <joeldbirch@gmail.com>
2018-04-10 17:52:43 +1000

    Reset config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,9 +23,9 @@ module.exports = {
           * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
           * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
           */
-        baseUrl: "fppdesignwp.dev.au",
+        baseUrl: "admin.fppdesign.com.au",
         // The protocol. This can be http or https.
-        protocol: "http",
+        protocol: "https",
         // Indicates whether the site is hosted on wordpress.com.
         // If false, then the asumption is made that the site is self hosted.
         // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
@@ -38,9 +38,9 @@ module.exports = {
         auth: {
           // If auth.user and auth.pass are filled, then the source plugin will be allowed
           // to access endpoints that are protected with .htaccess.
-          // htaccess_user: "cat",
-          // htaccess_pass: "seabird-away-bedside",
-          // htaccess_sendImmediately: false,
+          htaccess_user: "cat",
+          htaccess_pass: "seabird-away-bedside",
+          htaccess_sendImmediately: false,
 
           // If hostingWPCOM is true then you will need to communicate with wordpress.com API
           // in order to do that you need to create an app (of type Web) at https://developer.wordpress.com/apps/
