312d71dff40be4f9ac2b5fbe5746ade10d59bcb7
Joel Birch <joeldbirch@gmail.com>
2019-04-07 12:09:27 +1000

    Style new form.

--- src/templates/page.js
+++ src/templates/page.js
@@ -7,21 +7,15 @@ import BaseMainColumn from '../components/BaseMainColumn'
 import BaseSideColumn from '../components/BaseSideColumn'
 import SidebarWidgetFactory from '../components/SidebarWidgetFactory'
 import { sortByObjProp } from '../utils/helpers'
-import wufooForm from '../utils/wufooForm'
 
 class Template extends Component {
-  componentDidMount() {
-    let { slug } = this.props.data.wordpressPage
-    wufooForm(slug)
-  }
-
-  render() {
-    let { title, content, acf } = this.props.data.wordpressPage
-    let { siteMetadata } = this.props.data.site
-    let { sidebarItems } = this.props.pageContext
-    let sortedSidebarItems = sortByObjProp(acf.page_sidebar_items, sidebarItems, 'wordpress_id')
-    let pageTitle = [acf.page_title || title, siteMetadata.title].join(' | ')
-    let description = [acf.description || title, siteMetadata.title].join(' | ')
+  render () {
+    const { title, content, acf, slug } = this.props.data.wordpressPage
+    const { siteMetadata } = this.props.data.site
+    const { sidebarItems } = this.props.pageContext
+    const sortedSidebarItems = sortByObjProp(acf.page_sidebar_items, sidebarItems, 'wordpress_id')
+    const pageTitle = [acf.page_title || title, siteMetadata.title].join(' | ')
+    const description = [acf.description || title, siteMetadata.title].join(' | ')
 
     return (
       <Layout>
@@ -32,7 +26,9 @@ class Template extends Component {
           </Helmet>
 
           <TheHeading>{title}</TheHeading>
-          <BaseMainColumn>{content}</BaseMainColumn>
+          <BaseMainColumn>
+            {content}
+          </BaseMainColumn>
           <BaseSideColumn>
             <SidebarWidgetFactory nodes={sortedSidebarItems} />
           </BaseSideColumn>
