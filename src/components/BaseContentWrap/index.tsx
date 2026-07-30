bcfded1d0f4191c4e99c05ee88b187da5d972679
Joel Birch <joeldbirch@gmail.com>
2026-07-30 15:58:25 +1000

    styles migration

--- src/components/BaseContentWrap/index.tsx
+++ src/components/BaseContentWrap/index.tsx
@@ -2,7 +2,13 @@ import type { ReactNode } from 'react'
 import styles from './style.module.scss'
 
 export default ({ children }: { children?: ReactNode }) => (
-  <div className={styles.innerwrap}>
+  <div className={`
+    mx-auto
+    px-6
+    lap:px-11
+    w-full
+    desk:max-w-5xl
+  `}>
     <div className={`${styles.editable}  s-editable`}>
       {children}
     </div>
