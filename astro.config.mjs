b91b99ad7accc7214ec2c68bb62fa9bf358b591d
Joel Birch <joeldbirch@gmail.com>
2026-07-30 15:57:37 +1000

    Install and setup tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://fppdesign.com.au',
   trailingSlash: 'always',
   integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
